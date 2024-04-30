697cec75c860593192bc2fa198acab436d50555c
susvithareddy <163148969+susvithareddy@users.noreply.github.com>
2024-04-30 12:30:40 +0530

    Add files via upload

---
+++ Online-Quiz/js/questions.js
@@ -0,0 +1,222 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What is the purpose of CSS?",
+    answer: "To style HTML documents",
+    options: [
+        "To add functionality to HTML pages",
+        "To create animations",
+        "To style HTML documents",
+        "To store data"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which HTML tag is used to define an internal style sheet?",
+    answer: "style",
+    options: [
+        "css",
+        "script",
+        "style",
+        "link"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which property is used to change the background color?",
+    answer: "background-color",
+    options: [
+        "bgcolor",
+        "color",
+        "background-color",
+        "background"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which JavaScript function is used to write text to the console?",
+    answer: "console.log()",
+    options: [
+        "console.output()",
+        "console.log()",
+        "document.write()",
+        "alert()"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which HTML attribute is used to define inline styles?",
+    answer: "style",
+    options: [
+        "class",
+        "styles",
+        "style",
+        "font"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which property is used to change the text color of an element?",
+    answer: "color",
+    options: [
+        "text-color",
+        "fgcolor",
+        "color",
+        "background-color"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What does the 'document.querySelector' method return?",
+    answer: "The first element that matches a specified CSS selector(s)",
+    options: [
+        "All elements that match a specified CSS selector(s)",
+        "The first element that matches a specified CSS selector(s)",
+        "The last element that matches a specified CSS selector(s)",
+        "An array of all matched elements"
+    ]
+  },
+  {
+    numb: 13,
+    question: "How do you declare a JavaScript variable?",
+    answer: "var",
+    options: [
+        "var",
+        "variable",
+        "v",
+        "let"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What HTML tag is used for creating a drop-down list?",
+    answer: "select",
+    options: [
+        "input",
+        "list",
+        "drop-down",
+        "select"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which HTML element is used to display a scalar measurement within a range?",
+    answer: "meter",
+    options: [
+        "range",
+        "measure",
+        "gauge",
+        "meter"
+    ]
+  },
+  {
+    numb: 16,
+    question: "What attribute do you use to specify the source file in an HTML script element?",
+    answer: "src",
+    options: [
+        "href",
+        "src",
+        "type",
+        "file"
+    ]
+},
+{
+    numb: 17,
+    question: "Which CSS property controls the text size?",
+    answer: "font-size",
+    options: [
+        "text-style",
+        "font-size",
+        "text-size",
+        "font-weight"
+    ]
+},
+{
+    numb: 18,
+    question: "What is the correct HTML element for playing video files?",
+    answer: "video",
+    options: [
+        "media",
+        "video",
+        "movie",
+        "stream"
+    ]
+},
+{
+    numb: 19,
+    question: "In JavaScript, which object is used to handle dates?",
+    answer: "Date",
+    options: [
+        "Time",
+        "Date",
+        "Moment",
+        "DateTime"
+    ]
+},
+  {
+    numb: 20,
+    question: "Which CSS property creates an animated transition effect?",
+    answer: "transition",
+    options: [
+        "animate",
+        "transition",
+        "transfer",
+        "effect"
+    ]
+  }
+];
